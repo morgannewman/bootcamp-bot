65379b71747cd3edfb03cab02c7289f33aa75be9
Morgan Newman <mpnewman95@gmail.com>
2018-11-02 11:16:39 -0700

    Adds travis setup for deployment

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
-const dotenv = require('dotenv').config();
-const Discord = require('discord.js');
+const dotenv = require("dotenv").config();
+const http = require("http");
+const Discord = require("discord.js");
 const client = new Discord.Client();
-const CommandSystem = require('./command-system.js')();
+const CommandSystem = require("./command-system.js")();
 
-client.on('ready', () => {
-  console.log('Ready!');
+client.on("ready", () => {
+  console.log("Ready!");
 });
 
-client.on('message', message => {
+client.on("message", message => {
   CommandSystem.execute(client, message);
 });
 
 CommandSystem.load(function() {
-  console.log('Command system loaded.');
+  console.log("Command system loaded.");
 });
 
 client.login(process.env.TOKEN);
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/plan" });
+    res.write("Hello! This is a Discord bot. Nothing to see here.");
+    res.end();
+  })
+  .listen(process.env.PORT || 8080);
